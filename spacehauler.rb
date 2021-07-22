34b7b9deb2b7789be982a93c1a05ae2550928974
Joe Dupuis <joe@dupuis.io>
2021-07-22 11:04:05 -0700

    Username

--- spacehauler.rb
+++ spacehauler.rb
@@ -1,1 +1,8 @@
+#!/usr/bin/env ruby
+require './dotenv'
+require './game_client_example'
 puts 'hello'
+
+puts ENV['username'], ENV['password']
+client = GameClient.new
+client.login(ENV['username'], ENV['password'])
