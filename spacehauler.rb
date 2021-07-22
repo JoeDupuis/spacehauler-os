32b2d6228bb3780c95027c40db25920ce5c38da6
Joe Dupuis <joe@dupuis.io>
2021-07-22 11:06:29 -0700

    bundler

--- spacehauler.rb
+++ spacehauler.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
+require 'bundler'
+Bundler.require
 require './dotenv'
 require './game_client_example'
+
+
 puts 'hello'
 
-puts ENV['username'], ENV['password']
 client = GameClient.new
 client.login(ENV['username'], ENV['password'])
